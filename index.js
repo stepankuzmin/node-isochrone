9e2fa48682e503fcf123137d6b0ab7b5491c249a
George Lifchits <george.lifchits@gmail.com>
2017-07-11 14:21:11 -0400

    Implement deintersect as a configurable option

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ const makePolygons = (pointsByInterval, options) =>
  * @param {number} options.cellSize - the distance across each cell as in
  * [@turf/point-grid](https://github.com/Turfjs/turf/tree/master/packages/turf-point-grid)
  * @param {Array.<number>} options.intervals - intervals for isochrones in minutes
+ * @param {boolean} options.deintersect - whether or not to deintersect the isochrones.
+ * If this is true, then the isochrones will be mutually exclusive
  * @param {number} [options.concavity=2] - relative measure of concavity as in
  * [concaveman](https://github.com/mapbox/concaveman)
  * @param {number} [options.lengthThreshold=0] - length threshold as in
@@ -108,7 +110,7 @@ const isochrone = (startPoint, options) => {
         );
 
         const polygons = makePolygons(pointsByInterval, options);
-        const features = deintersect(polygons);
+        const features = options.deintersect ? deintersect(polygons) : polygons;
         const featureCollection = rewind(helpers.featureCollection(features));
         resolve(featureCollection);
       } catch (e) {

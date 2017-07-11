536020cfa44b7921f9ff1536de37a72a3fb9827e
George Lifchits <george.lifchits@gmail.com>
2017-07-11 11:26:29 -0400

    Add test to check that smaller isochrones are contained by larger ones
    Tests fail, this catches the bug described in urbica/galton#180

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const test = require('tape');
 const OSRM = require('osrm');
+const area = require('@turf/area');
+const intersect = require('@turf/intersect');
 const geojsonhint = require('@mapbox/geojsonhint');
 const isochrone = require('../index');
 
@@ -23,8 +25,19 @@ const options = {
   units: 'kilometers'
 };
 
+
+// For argument array intervals: [5, 10, 15, 20]
+// Gives: [ [5,10], [5,15], [5,20], [10,15], [10,20], [15,20] ]
+function increasingPairs(intervals) {
+  return intervals.map(i => ({ lhs: i, rhs: intervals.filter(x => x > i) }))
+    .map(z => z.rhs.map(r => [z.lhs, r]))
+    .reduce((x, y) => x.concat(y), []);
+}
+const testPairs = increasingPairs(options.intervals);
+
+
 test('isochrone', (t) => {
-  t.plan(3);
+  t.plan(points.length * (1 + testPairs.length));
   points.forEach(point =>
     isochrone(point, options)
       .then((geojson) => {
@@ -35,6 +48,26 @@ test('isochrone', (t) => {
         } else {
           t.pass('Valid GeoJSON');
         }
+        // Gives { interval1: isochrone1, interval2: isochrone2, ... }
+        const isochrones = options.intervals.reduce((acc, interval) => (
+          Object.assign({}, acc, {
+            [interval]: geojson.features
+              .find(iso => iso.properties.time === interval)
+          })), {}
+        );
+        // test that every smaller isochrone is contained by a larger one
+        testPairs.forEach((minutePair) => {
+          const [minSmall, minLarge] = minutePair;
+          const [small, large] = [isochrones[minSmall], isochrones[minLarge]];
+          const intersection = intersect(small, large);
+          const areaIntersection = intersection ? area(intersection) : 0;
+          // assert that the small isochrone overlaps over 90% with the large
+          if ((areaIntersection / area(small)) > 0.9) {
+            t.pass(`Isochrone ${minSmall} is contained in ${minLarge}`);
+          } else {
+            t.fail(`Isochrone ${minSmall} is not contained in ${minLarge}`);
+          }
+        });
       })
       .catch(error => t.error(error, 'No error'))
   );
